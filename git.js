fc454ad143ca36ec47ec6983f820f6d67b09be01
Lyuz Anthony <lyuz.previatto@aluno.senai.br>
2025-04-08 10:50:32 -0300

    branch

--- git.js
+++ git.js
@@ -3,4 +3,7 @@ console.log("todas as comidas", comidas);
 
 comidas.sort()
 console.log('comidas ordenadas: ', comidas);
+console.log('comidas ordenadas: ', comidas);
+console.log('teste mesclar branch');
+
 
