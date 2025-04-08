f627843914be99152ec9a3e303d985ecd4dbdbf9
Lyuz Anthony <lyuz.previatto@aluno.senai.br>
2025-04-08 10:20:26 -0300

    alteração do git.js

--- git.js
+++ git.js
@@ -1,2 +1,6 @@
 let comidas = ["lasanha", "Pizza", "Sushi", "Hamburguer", "Batata frita"]
 console.log("todas as comidas", comidas);
+
+comidas.sort()
+console.log('comidas ordenadas: ', comidas);
+
